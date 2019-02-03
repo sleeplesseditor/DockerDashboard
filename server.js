4c0c20be80979984fd00016ee783fc1f6437744d
sleeplesseditor <josh@bryson.me.uk>
2019-02-03 14:06:54 +0000

    Webpack setup

--- server.js
+++ server.js
@@ -3,10 +3,10 @@ let path = require('path')
 let app = express()
 let server = require('http').Server(app)
 
-let port = process.env.PORT || 3000
+let port = process.env.PORT || 3001
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
 
-server.listen(port, () => console.log(`Server started on port ${port}`))
+server.listen(port, () => console.log(`Server started on Port ${port}`))
