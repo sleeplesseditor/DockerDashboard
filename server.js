738377c1448b84ed7a8045400bd07bdee0ba6a5d
sleeplesseditor <josh@bryson.me.uk>
2019-02-03 10:53:53 +0000

    Initial server setup

---
+++ server.js
@@ -0,0 +1,12 @@
+let express = require('express')
+let path = require('path')
+let app = express()
+let server = require('http').Server(app)
+
+let port = process.env.PORT || 3000
+
+app.use(express.static('public'))
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')))
+
+server.listen(port, () => console.log(`Server started on port ${port}`))
